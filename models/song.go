368e043902f544378d3daace438226df5c88cf1d
Christina Kozanian <christinakozanian@gmail.com>
2024-09-28 17:46:49 -0700

    adds controllers and routes packages

--- models/song.go
+++ models/song.go
@@ -1,6 +1,7 @@
 package models
 
 type Song struct {
+	ID       string  `json:"id" bson:"_id,omitempty"`
 	Name     string  `json:"name" bson:"name"`
 	Artist   string  `json:"artist" bson:"artist"`
 	Duration float32 `json:"duration" bson:"duration"`
